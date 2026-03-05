41e833087c51ab8dc5b83351563cf2c843612986
Retr0-505 <vnepomuc@stedwards.edu>
2026-03-05 00:11:32 -0600

    12am update
    Added a folder containing all scripts
    Added Zombies that follow player
    
    need to add health system

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log("Player Health: " + health);
+
+        if (health <= 0)
+        {
+            Debug.Log("Player Dead");
+        }
+    }
+}
