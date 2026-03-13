05bbf8a94050f9e4b6269cd966d4c0d0c92f98e4
B1ackhat03 <153757822+B1ackhat03@users.noreply.github.com>
2026-03-13 15:47:44 -0500

    Controller Support
    Added Shooting to controller previos was moving and looking support

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -1,55 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GunScript : MonoBehaviour
 {
     // Where the laser starts (tip of the gun)
     public Transform firePoint;
-
     // How far the railgun can shoot
     public float laserDistance = 100f;
-
     // Time between shots (cooldown)
     public float shootCooldown = 3f;
-
     // The visible laser beam
     public LineRenderer laserLine;
-
     // Tracks when we are allowed to shoot again
     private float nextShootTime = 0f;
-
     // Lets the shoot Indicator know if we can shoot
     public bool canShoot
     {
-        get {
+        get
+        {
             return Time.time >= nextShootTime;
         }
     }
-
     void Update()
     {
-        // Left mouse button
-        if (Input.GetButtonDown("Fire1"))
+        // Left mouse button (Fire1) OR Xbox One Right Trigger (Fire2)
+        bool firePressed = Input.GetButtonDown("Fire1") || Input.GetAxisRaw("Fire2") > 0.1f;
+
+        if (firePressed)
         {
             // Check if cooldown is finished
             if (Time.time >= nextShootTime)
             {
                 ShootRailGun();
-
                 // Start cooldown timer
                 nextShootTime = Time.time + shootCooldown;
             }
         }
     }
-
     void ShootRailGun()
     {
         // This shoots a ray that detects EVERYTHING in a line
         RaycastHit[] hits = Physics.RaycastAll(firePoint.position, firePoint.forward, laserDistance);
-
         Vector3 endPoint = firePoint.position + firePoint.forward * laserDistance;
-
         // Loop through everything the laser hits
         foreach (RaycastHit hit in hits)
         {
@@ -59,27 +51,21 @@ public class GunScript : MonoBehaviour
                 // Destroy the zombie instantly
                 Destroy(hit.collider.gameObject);
             }
-
             // Set beam end point to the farthest hit
             endPoint = hit.point;
         }
-
         // Draw the laser beam
         StartCoroutine(DrawLaser(firePoint.position, endPoint));
     }
-
     IEnumerator DrawLaser(Vector3 start, Vector3 end)
     {
         // Turn the laser on
         laserLine.enabled = true;
-
         // Set the start and end of the beam
         laserLine.SetPosition(0, start);
         laserLine.SetPosition(1, end);
-
         // Keep the beam visible briefly
         yield return new WaitForSeconds(0.1f);
-
         // Turn it off
         laserLine.enabled = false;
     }
